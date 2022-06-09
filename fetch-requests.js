307240b302d3ab22e4082f0610075679e91183c3
Derek Emsbach <demsbach92@gmail.com>
2022-06-08 21:35:09 -0600

    Phase 1 and 2

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,13 +6,59 @@ Paste your code for fetch requests here once you finish each phase.
 /* ============================== Phase 1 ============================== */
 
 // Your code here
+const fetch('/products', {
+  method : "POST",
+  header : {
+      "Content-Type" : "application/x-www-form-urlencoded"
+  },
+  body : new URLSearchParams({
+      "name" : 'Plumbus',
+      "description" : 'Rick & Morty',
+      "price" : 21.09,
+      "categories" : "grocery"
+  })
 
+})
 
 
 /* ============================== Phase 2 ============================== */
 
 // Your code here
+fetch('/products', {
+  method : "POST",
+  header : {
+      "Content-Type" : "application/x-www-form-urlencoded"
+  },
+  body : new URLSearchParams({
+      "name" : 'Caribbean Delight Coffee',
+      "description" : 'Made by Manatee Coffee',
+      "price" : 11.09,
+      "categories" : "grocery"
+  })
 
+}).then((resObj) => {
+  console.log(resObj.status)
+  return resObj
+})
+.then(res => console.log(res))
+
+
+
+fetch('/products', {
+  method : "POST",
+  header : {
+      "Content-Type" : "application/x-www-form-urlencoded"
+  },
+  body : new URLSearchParams({
+      "name" : 'Caribbean Delight Coffee',
+      "description" : 'Made by Manatee Coffee',
+      "price" : 11.09,
+      "categories" : "grocery"
+  })
+
+}).then((resObj) => {
+  return resObj.text()
+}).then((resObj) => console.log(resObj))
 
 
 /* ============================== Phase 3 ============================== */
